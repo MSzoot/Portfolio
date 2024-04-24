f1b66170b556bc8f68ca9465f6301bc1b8b5ddbd
MSzoot <97002088+MSzoot@users.noreply.github.com>
2024-04-24 14:11:09 +0100

    grammar

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -7,14 +7,14 @@ export default function Hero() {
         <div className="w-10/12">
           <h1 className="text-5xl font-bold">Hello!</h1>
           <p className="py-6">
-            I'm Martin, an IT technician based in Ipswich UK with an enduring
+            I'm Martin, an IT Profesional based in Ipswich with an enduring
             passion for all things related to computer tinkering. Recently, I've
             discovered a new passion: coding. This exciting journey has ignited
             my creativity and fueled my drive to excel in the realm of
             programming.
           </p>
           <a className="btn-primary btn" href="#contact">
-            Contact me{" "}
+            Contact me
           </a>
         </div>
         <img src={msphoto} className="mt-10 max-w-xs md:max-w-md" />
