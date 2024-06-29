2510fe13d16cfbd22cd790c1734f5d7bdf39a031
MSzoot <97002088+MSzoot@users.noreply.github.com>
2024-06-29 19:28:29 +0100

    content

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -5,8 +5,9 @@ export default function Portfolio() {
     <div className="mx-auto mt-32 w-10/12" id="projects">
       <h2 className="mb-10 text-center text-4xl font-bold">Projects</h2>
       <p className="mb-10 text-center">
-        These are the projects I'm most proud of, showcasing my skills and
-        creativity.
+        These projects highlight my skills and creativity, showcasing the work
+        I'm most proud of. Additional smaller learning projects can be found on
+        my GitHub.
       </p>
       <div className="grid justify-center gap-12 lg:grid-cols-2">{Cards}</div>
     </div>
