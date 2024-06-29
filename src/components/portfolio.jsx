cc7e8468dc6b0a431b8f5cab7a23f9c0ace62db4
MSzoot <97002088+MSzoot@users.noreply.github.com>
2024-06-29 19:06:26 +0100

    content up

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -6,9 +6,7 @@ export default function Portfolio() {
       <h2 className="mb-10 text-center text-4xl font-bold">Projects</h2>
       <p className="mb-10 text-center">
         These are the projects I'm most proud of, showcasing my skills and
-        creativity. I've chosen to exclude standard tutorial projects like
-        weather apps and to-do lists. If you’re interested in exploring more of
-        my code, you can find those on my GitHub.
+        creativity.
       </p>
       <div className="grid justify-center gap-12 lg:grid-cols-2">{Cards}</div>
     </div>
